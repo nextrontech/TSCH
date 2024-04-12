708f5dfe3f803184be50f28412b783035190e7f2
nextrontech <166491996+nextrontech@users.noreply.github.com>
2024-04-12 13:41:03 +0300

    Add files via upload

---
+++ res/js/app.js
@@ -0,0 +1,22 @@
+const Xbtn = document.querySelector(".sidebar i");
+const Sbar = document.querySelector(".sidebar");
+const Mbtn = document.querySelector(".menu i");
+const parawords = document.querySelector(".container-02 .para p");
+
+const text = `By empowering disadvantaged youth through education, nutrition,
+          protection, we support to breaking the cycle of poverty and build
+          brighter futures for them, they deserve everything you have . . .`;
+
+Xbtn.onclick = () => {
+  Sbar.style.display = "none";
+};
+
+Mbtn.onclick = () => {
+  Sbar.style.display = "flex";
+};
+
+for (let i = 0; i < text.length; i++) {
+  setTimeout(() => {
+    parawords.append(text[i]);
+  }, 50 * i);
+}
